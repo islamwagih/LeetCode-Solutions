55329ac33b00b2c1e3e60239df18868cfd1c9f2a
Islam Wagih <islamwagihe@gmail.com>
2022-08-09 18:58:55 +0200

    update

---
+++ Smallest Range Covering Elements from K Lists.cpp
@@ -0,0 +1,41 @@
+class Solution {
+public:
+    vector<int> smallestRange(vector<vector<int>>& nums) {
+        if(nums.size() == 1){
+            vector<int> vec;
+            vec.push_back(nums[0][0]);
+            vec.push_back(nums[0][0]);
+            return vec;
+        }
+        set<pair<int, pair<int, int>>> st;
+        for(int i=0;i<nums.size();i++){
+            pair<int, pair<int, int>> pr;
+            pr.first = nums[i][0];
+            pr.second.first = i;
+            pr.second.second = 0;
+            st.insert(pr);
+        }
+        int minx = 0, miny = INT_MAX;
+        while(st.size()){
+            pair<int, pair<int, int>> minPr = *st.begin(), maxPr = *st.rbegin();
+            int minElem = minPr.first, maxElem = maxPr.first;
+            int arrInd = minPr.second.first;
+            int minElemInd = minPr.second.second;
+            if((maxElem - minElem) < (miny-minx)){
+                minx = minElem;
+                miny = maxElem;
+            }
+            if((nums[arrInd].size()-1) == minElemInd) break;
+            st.erase(minPr);
+            pair<int, pair<int, int>> toPush;
+            toPush.first = nums[arrInd][minElemInd+1];
+            toPush.second.first = arrInd;
+            toPush.second.second = minElemInd+1;
+            st.insert(toPush);
+        }
+        vector<int> vec(2);
+        vec[0] = minx;
+        vec[1] = miny;
+        return vec;
+    }
+};
