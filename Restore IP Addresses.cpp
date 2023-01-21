fec77db3a6e84120652f880fdaeef25620c8f56d
Islam Wagih <islamwagihe@gmail.com>
2023-01-21 20:02:58 +0200

    update

--- Restore IP Addresses.cpp
+++ Restore IP Addresses.cpp
@@ -2,11 +2,49 @@ class Solution
 {
     vector<string> ips;
     string s;
+
+    int intLen(int x)
+    {
+        if(x == 0) return 1;
+        int len = 0;
+        while(x)
+        {
+            len++;
+            x/=10;
+        }
+        return len;
+    }
+
+    int strToInt(int l, int r, string& s)
+    {
+        int x = 0;
+        for(int i=l;i<=r;i++)
+        {
+            x *= 10;
+            x += (s[i]-'0');
+        }
+        return x;
+    }
+
+    bool validIP(string& s)
+    {
+        int i = 0, n = s.size();
+        while(i < n)
+        {
+            int j=i+1;
+            while(j < n && s[j] != '.') j++;
+            int val = strToInt(i, j-1, s), sLen = j-i, ilen = intLen(val);
+            if(ilen != sLen || val > 255) return false;
+            i = j+1;
+        }
+        return true;
+    }
+
     void dotSeparator(int i, int subLen, string ip, int dots)
     {
         if(i == s.size())
         {
-            if(dots == 3) ips.push_back(ip);
+            if(dots == 3 && validIP(ip)) ips.push_back(ip);
             return;
         }
         //extend if we can
@@ -21,40 +59,6 @@ class Solution
         }
     }
 
-    int intLen(int x)
-    {
-        if(x == 0) return 1;
-        int len = 0;
-        while(x)
-        {
-            len++;
-            x/=10;
-        }
-        return len;
-    }
-    int strToInt(int l, int r, string& s)
-    {
-        int x = 0;
-        for(int i=l;i<=r;i++)
-        {
-            x *= 10;
-            x += (s[i]-'0');
-        }
-        return x;
-    }
-    bool validIP(string& s)
-    {
-        int i = 0, n = s.size();
-        while(i < n)
-        {
-            int j=i+1;
-            while(j < n && s[j] != '.') j++;
-            int val = strToInt(i, j-1, s), sLen = j-i, ilen = intLen(val);
-            if(ilen != sLen || val > 255) return false;
-            i = j+1;
-        }
-        return true;
-    }
 public:
     vector<string> restoreIpAddresses(string s)
     {
@@ -62,11 +66,6 @@ public:
         if(n < 4 || n > 12) return {};
         this->s = s;
         dotSeparator(0, 0, "", 0);
-        vector<string> validIps;
-        for(string& s:ips)
-        {
-            if(validIP(s)) validIps.push_back(s);
-        }
-        return validIps;
+        return ips;
     }
 };
