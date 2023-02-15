52b1613b0829b8613ea2f6eaa4e984564deefb31
Islam Wagih <islamwagihe@gmail.com>
2023-02-15 20:15:23 +0200

    update

--- Add to Array-Form of Integer.cpp
+++ Add to Array-Form of Integer.cpp
@@ -1,41 +1,18 @@
 class Solution {
 public:
-    vector<int> getSum(vector<int>& a, vector<int>& b)
+    vector<int> addToArrayForm(vector<int>& num,  int k)
     {
-        //ensure that a size will always be bigger than b size
-        if(a.size() < b.size()) swap(a, b);
-        vector<int> sum(a.size()+1, -1);
-        for(int i=0;i<b.size();i++)
+        vector<int> sum;
+        int runningSum = k, n = num.size();
+        int i = n-1;
+        while(i >= 0 || runningSum > 0)
         {
-            sum[i] = a[i] + b[i];
+            if(i >= 0) runningSum += num[i];
+            sum.push_back(runningSum%10);
+            runningSum /= 10;
+            i--;
         }
-        for(int i=b.size();i<a.size();i++)
-        {
-            sum[i] = a[i];
-        }
-        for(int i=0;i<a.size();i++)
-        {
-            if(sum[i] >= 10)
-            {
-                sum[i] -= 10;
-                if(sum[i+1] == -1) sum[i+1] = 0;
-                sum[i+1]++;
-            }
-        }
-        if(sum[sum.size()-1] == -1) sum.pop_back();
         reverse(sum.begin(), sum.end());
         return sum;
-
-    }
-    vector<int> addToArrayForm(vector<int>& a, int k)
-    {
-        vector<int> b;
-        while(k)
-        {
-            b.push_back(k%10);
-            k /= 10;
-        }
-        reverse(a.begin(), a.end());
-        return getSum(a, b);
     }
 };
