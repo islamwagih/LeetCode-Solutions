06aad713dc68f8a29cf987d53a943635692aea3f
Islam Wagih <islamwagihe@gmail.com>
2023-02-22 13:52:06 +0200

    update

---
+++ Single Element in a Sorted Array.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums)
+    {
+        int left = 0, right = nums.size() - 1;
+        while(left < right - 1)
+        {
+            int mid = (left + right)/2;
+            if(mid % 2 == 1) mid--;
+            if(nums[mid] != nums[mid+1])
+            {
+                right = mid;
+            }else
+            {
+                left = mid + 2;
+            }
+        }
+        return nums[left];
+    }
+};
