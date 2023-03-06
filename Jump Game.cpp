dc0e4bb3f56ed6a0087c3058bb6bc757b14c8700
Islam Wagih <islamwagihe@gmail.com>
2023-03-06 17:42:14 +0200

    update

---
+++ Jump Game.cpp
@@ -0,0 +1,13 @@
+class Solution
+{
+public:
+    bool canJump(vector<int>& nums)
+    {
+       int n = nums.size(), last = n-1;
+       for(int i=n-2;i>=0;i--)
+       {
+           if(i + nums[i] >= last) last = i;
+       }
+       return last == 0;
+    }
+};
