3f55a807d2f9d3fc0ab7acc7dac3b36bf4bc2793
Islam Wagih <islamwagihe@gmail.com>
2023-04-26 15:31:14 +0200

    update

---
+++ Product of Array Except Self.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    vector<int> productExceptSelf(vector<int>& nums)
+    {
+        int n = nums.size();
+        vector<int> ans(n);
+        int prefix = 1;
+        for(int i=0;i<n;i++)
+        {
+            ans[i] = prefix;
+            prefix *= nums[i];
+        }
+        int suffix = 1;
+        for(int i=n-1;i>=0;i--)
+        {
+            ans[i] *= suffix;
+            suffix *= nums[i];
+        }
+        return ans;
+    }
+};
